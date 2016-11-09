8f48f9d9b6e576b9d430ff2bf1cd7635a517d5ff
Shreyansh Nahata <shreyans4nahata@gmail.com>
2016-11-09 19:22:50 +0530

    Fixed adding question

--- public/src/addques/AddQuesController.js
+++ public/src/addques/AddQuesController.js
@@ -1,16 +1,30 @@
 (function () {
     'use strict';
     angular.module('miniRvce')
-        .controller('AddQuesController', function($rootScope, $window, $stateParams,AddQues) {
+        .controller('AddQuesController', function($rootScope, $window, $stateParams,AddQues,Tag) {
             var self = this;
 
             self.user = null;
-            
+
+            self.tagsList = [];
+            Tag.getTags()
+                .then(function (response) {
+                    //console.log(response);
+                    self.tagsList = response.data.TAGS;
+                }).catch(function(reason){
+                    console.log("Tag retrieval error: ",reason);
+                });
+
             self.user = $window.localStorage.getItem('user_id');
             self.uname = $window.localStorage.getItem('name');
             self.col_id = $window.localStorage.getItem('col_id');
+            self.tag_id = null;
 
             var ques_data = {};
+
+            self.getTag = function(val){
+                self.tag_id = val;
+            };
             self.addQuestion = function () {
                 console.log("tag_id ",self.tag_id);
                 ques_data = {
